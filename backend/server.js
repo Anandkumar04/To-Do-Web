052596693c9e4e185b190b446253949b68c2e99b
Anandkumar04 <146409586+Anandkumar04@users.noreply.github.com>
2025-04-05 21:55:06 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,13 +11,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb+srv://Anand24:Anand24@cluster0.n5mskdu.mongodb.net/Todo-app?retryWrites=true&w=majority&appName=Cluster0", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log("MongoDB connected successfully"))
-.catch((err) => console.error("MongoDB connection error:", err));
-
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected successfully"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 app.use("/api/tasks", taskRoutes);
 
